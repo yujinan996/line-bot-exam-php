fc6c11835f86ff177a760f0674655805e40e3042
yujinan996 <46959003+yujinan996@users.noreply.github.com>
2019-01-23 17:17:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '4RhrtZYSr+yA9aVT03gz5yI8FqZvLApzmZ5rIGoKZppXGbazszGSjGq8ON2hhyIbTmQ4CPAXWXyvvJ3OePP+LV3qLlcX618ijkSsc0tgSSxwr1xQ0i7zbBn1oif+hfW9CQWoOfKxsQfEEf1jrwvu5AdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
