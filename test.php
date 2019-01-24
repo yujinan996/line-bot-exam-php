a9ccc7132917da7a445a85cd51a5c775d9650550
yujinan996 <46959003+yujinan996@users.noreply.github.com>
2019-01-24 16:25:40 +0700

    Update test.php

--- test.php
+++ test.php
@@ -26,8 +26,8 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Image"
-    else if($message == "รูปน้องแมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+    else if($message == "ยูจิน"){
+        $image_url = "https://img.kpopmap.com/2018/10/DftVz6cVMAMyYY0.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
